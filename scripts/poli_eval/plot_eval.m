ffa9039f5c6c4e736a82664169dd9e673fd81a52
Jason <jasonpeng142@hotmail.com>
2015-11-09 18:52:22 -0800

    getting reid of some intermediate files

--- scripts/poli_eval/plot_eval.m
+++ scripts/poli_eval/plot_eval.m
@@ -10,11 +10,15 @@ n = min(n, length(nn_vel_data));
 qp_plot_vals = ones(n, 1);
 qp_plot_vals = qp_plot_vals .* qp_data(1);
 
-plot(qp_plot_vals, 'k-');
+nn_torque_data = nn_torque_data(1:n);
+nn_pd_data = nn_pd_data(1:n);
+nn_vel_data = nn_vel_data(1:n);
+
+plot(qp_plot_vals, 'k--');
 hold on;
-plot(nn_torque_data(1:n), 'b-');
-plot(nn_pd_data(1:n), 'r-');
-plot(nn_vel_data(1:n), 'g-');
+plot(nn_torque_data, 'b-');
+plot(nn_pd_data, 'r-');
+plot(nn_vel_data, 'k-');
 hold off;
 
 xlabel('Iterations (10^3)');
