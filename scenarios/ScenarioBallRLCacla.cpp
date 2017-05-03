a9b35c6333bfed3c336adca7552d35e4c25fefe3
Jason <jasonpeng142@hotmail.com>
2017-05-03 14:29:50 -0700

    refactoring kernele evaluation for stoch nets

--- scenarios/ScenarioBallRLCacla.cpp
+++ scenarios/ScenarioBallRLCacla.cpp
@@ -105,7 +105,7 @@ void cScenarioBallRLCacla::InitTrainer()
 	mTrainerParams.mPlaybackMemSize = gTrainerPlaybackMemSize;
 	mTrainerParams.mPoolSize = 1;
 	mTrainerParams.mNumInitSamples = 10000;
-	//mTrainerParams.mNumInitSamples = 100;
+	mTrainerParams.mNumInitSamples = 100;
 	//mTrainerParams.mFreezeTargetIters = 100;
 
 	//mTrainerParams.mPGEnableImportanceSampling = true;
